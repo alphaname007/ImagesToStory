868673a24a8f4159c3c05ae9848b8db8448c5ab8
Daniel Weissenberger <116844918+alphaname007@users.noreply.github.com>
2024-10-10 17:28:05 +0200

    Add files via upload

---
+++ main_cmd.py
@@ -0,0 +1,28 @@
+import os
+
+from functions.SetupHandler import *
+from functions.FileHandler import *
+from functions.Orchastrator import *
+
+
+#main loop
+def main():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+    print(">> Images to Story <<")
+    media_directory_path = input("Media-Folder: ")
+    text = input("Story-Text: ")
+
+    if (os.path.exists(media_directory_path)):
+        print("\t> Ready to start generation")
+        generate_Episode(media_directory_path, text)
+    else:
+         print("\t> I am sorry but this folder does not exist")
+
+    input("Hit enter to continue")
+
+
+if __name__ == "__main__":
+    init()
+    while True:
+        main()
